d12d732fefab06680a97b02e2d4bd7a0d0c02a1d
EnyaChang2002 <yyy2011taiwan@gmail.com>
2024-03-14 20:03:20 +0800

    Prepare hw

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction new1 = new Fraction();
+        Fraction new2 = new Fraction(6);
+        Fraction new3 = new Fraction(6, 7);
+        
+        Fraction new4 = new Fraction();
+        //new4.SetTop(3);
+        //new4.SetBottom(4);
+        //string top = Console.ReadLine();
+        Console.WriteLine(new4.GetFractionString());
+        Console.WriteLine(new4.GetDecimalValue());
     }
 }
