86e8670ab650a4b935741bd6a4570a58ea22cd58
EnyaChang2002 <yyy2011taiwan@gmail.com>
2024-03-28 15:04:28 +0800

    prepare5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square newS = new Square("blue", 4);
+        Console.WriteLine(newS.GetColor());
+        Console.WriteLine(newS.GetArea());
+        Console.WriteLine();
+
+        Rectangle newR = new Rectangle("yellow", 6, 9);
+        Console.WriteLine(newR.GetColor());
+        Console.WriteLine(newR.GetArea());
+        Console.WriteLine();
+
+        Circle newC = new Circle("orange", 5);
+        Console.WriteLine(newC.GetColor());
+        Console.WriteLine(newC.GetArea());
+
+        List<Shape> newL = new List<Shape>();
+        newL.Add(newS);
+        newL.Add(newR);
+        newL.Add(newC);
+
+        foreach (Shape shape in newL)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
